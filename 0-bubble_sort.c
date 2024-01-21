6db0ef8913f5a9533f57118ea1c56574827808a9
kiptoo haron <kiptooharon.hk@gmail.com>
2024-01-21 07:33:09 +0300

    bubble sort algorithm

---
+++ 0-bubble_sort.c
@@ -0,0 +1,30 @@
+#include "sort.h"
+/**
+* bubble_sort - bubble sort algorithm, stps through each element of the list
+* as it compares th element with the previous element if need be, it swaps the
+* elements according to comparison operator
+* @array: list to be sorted
+* @size: size of the list to be sorted
+*/
+void bubble_sort(int *array, size_t size)
+{
+	bool swap = false;
+	int i, j, len = size, tmp;
+
+	for (i = 0; i < len - 1; i++)
+	{
+		for (j = 0; j < len - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				tmp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = tmp;
+				swap = true;
+				print_array(array, size);
+			}
+		}
+		if (swap == false)
+			break;
+	}
+}
